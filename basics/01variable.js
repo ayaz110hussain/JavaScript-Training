6db668fd8c08e0a7227a1bcdfb9e8d44b786d52f
Syed Ayaz Hussain Abidi <ayaz110hussain@gmail.com>
2026-06-03 20:50:02 +0000

    datatypes

---
+++ basics/01variable.js
@@ -0,0 +1,14 @@
+const accountId = 110110
+let accountEmail = "ayaz110hussain@gmail.com"
+var accountPassword = "110110"
+accountCity = "Mumbai"
+let accountState;
+
+console.log(accountId);
+
+accountEmail = "ayaz@ayaz.com"
+accountPassword = "110786"
+accountCity = "Bengaluru"
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+
---
+++ basics/02dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3)// we are using nodejs, not browser
+//console.log(10+10)
+console.log("Syed Ayaz")
+
+let name = "Ayaz"
+let age = 32
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigInt => for large number
+//string => "" double quotes
+//boolean => true/false
+//null => standalone value (empty value)
+//undefine => value not assign
+//symbol => unique
+
+//object   
+
+console.log(typeof age)
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
