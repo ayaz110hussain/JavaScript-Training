d0a04d41a88047f5814af8e46758d88d2d453c3e
Syed Ayaz Hussain Abidi <ayaz110hussain@gmail.com>
2026-06-22 19:15:10 +0000

    datatype summary

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+//Primitive
+
+//7 Categories : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotheId = Symbol('123')
+
+console.log(id === anotheId);
+
+const bigNumber = 4214612412n
+
+
+
+//Reference (Non Primitive)
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Ayaz",
+    age: 32,
+}
+
+const myFunction = function (){
+console.log("Hello World");
+}
+
+console.log(typeof myFunction);
